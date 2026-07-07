efe0697cd191e0b5a9d41fbe7198852a07604cc7
YanagiharaNaoya <>
2026-07-07 19:28:44 +0900

    授業21回目のコード

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -2,7 +2,6 @@ package raisetech.student.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,10 +10,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "heiio world";
 	}
 }
